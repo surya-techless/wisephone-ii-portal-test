1f02bb3b4c0da9bf50e6ab6b805bf6343175152a
Cameron Pak <cameronandrewpak@gmail.com>
2025-01-31 16:33:37 -0600

    Update Tailwind config and global styles for form inputs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -19,12 +19,11 @@ export default {
           secondary: "#f5f5f4",
           accent: "#6d28d9",
           neutral: "#d1d5db",
-          "base-100": "#f3f4f6",
+          "base-100": "#FFFFFF",
           info: "#a5f3fc",
           success: "#86efac",
           warning: "#fca5a5",
-          error: "#fb7185",
-          "--rounded-btn": "99px"
+          error: "#fb7185"
         }
       }
     ]
