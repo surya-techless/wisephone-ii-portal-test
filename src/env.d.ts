521aa439d75fe03a201633a88d50f986313d8b33
Cameron Pak <cameronandrewpak@gmail.com>
2025-02-01 06:31:51 -0600

    Remove deprecated environment variables and update Knox API configuration

--- src/env.d.ts
+++ src/env.d.ts
@@ -37,16 +37,6 @@ interface ImportMetaEnv {
   readonly PUBLIC_CLERK_PUBLISHABLE_KEY: string;
   /** https://clerk.com/docs/deployments/clerk-environment-variables#clerk-environment-variables */
   readonly CLERK_SECRET_KEY: string;
-  /** https://astro.cloudinary.dev */
-  readonly PUBLIC_CLOUDINARY_CLOUD_NAME: string;
-  /** https://astro.cloudinary.dev */
-  readonly PUBLIC_CLOUDINARY_API_KEY: string;
-  /** https://astro.cloudinary.dev */
-  readonly CLOUDINARY_API_SECRET: string;
-  /** https://astro.cloudinary.dev */
-  readonly PUBLIC_ASSETS_DIRECTORY: string;
-  /** https://groq.com/ */
-  readonly GROQ_API_KEY: string;
   /** https://docs.samsungknox.com/dev/knox-manage/api */
   readonly KNOX_CLIENT_ID: string;
   /** https://docs.samsungknox.com/dev/knox-manage/api */
