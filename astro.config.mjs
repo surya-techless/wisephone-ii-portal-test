2c2d98901dd86e7bc65fe3f03a9cd7cccae452a4
Cameron Pak <cameronandrewpak@gmail.com>
2025-02-01 06:51:07 -0600

    Remove Lottie integration from Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,8 @@ import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import netlify from "@astrojs/netlify";
-import sentry from "@sentry/astro";
 import db from "@astrojs/db";
 import clerk from "@clerk/astro";
-import lottie from "astro-integration-lottie";
-
-const SENTRY_DSN = import.meta.env.SENTRY_DSN;
-const SENTRY_AUTH_TOKEN = import.meta.env.SENTRY_AUTH_TOKEN;
-const SENTRY_PROJECT = import.meta.env.SENTRY_PROJECT;
-const isSentryEnabled = SENTRY_DSN && SENTRY_AUTH_TOKEN && SENTRY_PROJECT;
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,8 +14,7 @@ export default defineConfig({
     alpinejs({
       entrypoint: "/src/entrypoint"
     }),
-    clerk(),
-    lottie()
+    clerk()
   ],
   vite: {
     optimizeDeps: {
