5b08679f2fc2ceced713c4abe53784c4e0f7c3fd
Cameron Pak <cameronandrewpak@gmail.com>
2025-01-31 18:53:16 -0600

    Refactor feature seeding by extracting feature data to a shared constant

--- db/seed.ts
+++ db/seed.ts
@@ -1,4 +1,5 @@
-import { db, Features, Wisephone, WisephoneFeatures } from "astro:db";
+import { db, Features, Wisephone } from "astro:db";
+import { WISEPHONE_FEATURES } from "../src/libs/utils";
 
 // https://astro.build/db/seed
 export default async function seed() {
@@ -11,28 +12,5 @@ export default async function seed() {
     }
   ]);
 
-  await db.insert(Features).values([
-    {
-      knoxManageId: "300830f61c574b9db37dabbae1e79aa9",
-      lucideIcon: "wrench",
-      isEnabled: true,
-      name: "Enable Tool Drawer",
-      description: "Get access to safe, vetted third-party apps that are tools to help you get a job done."
-    },
-    {
-      isEnabled: true,
-      knoxManageId: "5ae84219a2ea4372a42f9da57c00d325",
-      lucideIcon: "fish-symbol",
-      name: "Enable faith.tools",
-      description:
-        "Get access to your favorite faith-based apps on the Tool Drawer, like the YouVersion Bible app and Hallow Prayer app."
-    },
-    {
-      isEnabled: true,
-      knoxManageId: "f463ade53b1f43f5bf2f540a99395c2d",
-      lucideIcon: "signal",
-      name: "Disable Hotspot",
-      description: "Disable the hotspot feature on your device."
-    }
-  ]);
+  await db.insert(Features).values(WISEPHONE_FEATURES);
 }
--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -23,3 +23,28 @@ export const setupKeyboardNavigation = () => {
     navigateByKeyboard(event);
   });
 };
+
+export const WISEPHONE_FEATURES = [
+  {
+    knoxManageId: "300830f61c574b9db37dabbae1e79aa9",
+    lucideIcon: "wrench",
+    isEnabled: true,
+    name: "Enable Tool Drawer",
+    description: "Get access to safe, vetted third-party apps that are tools to help you get a job done."
+  },
+  {
+    isEnabled: true,
+    knoxManageId: "5ae84219a2ea4372a42f9da57c00d325",
+    lucideIcon: "fish-symbol",
+    name: "Enable faith.tools",
+    description:
+      "Get access to your favorite faith-based apps on the Tool Drawer, like the YouVersion Bible app and Hallow Prayer app."
+  },
+  {
+    isEnabled: true,
+    knoxManageId: "f463ade53b1f43f5bf2f540a99395c2d",
+    lucideIcon: "signal",
+    name: "Disable Hotspot",
+    description: "Disable the hotspot feature on your device."
+  }
+];
